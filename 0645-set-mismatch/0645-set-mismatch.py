7269e6aa6f7e9e912126e5e4129f6035f66acc5a
SACHIN MR <221297219+Sachin-MR05@users.noreply.github.com>
2026-03-17 09:21:36 +0530

    6 ms (87.59%) 20.9 MB (43.53%) 0645-set-mismatch .py Easy

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,20 +1,11 @@
 class Solution:
-    def findErrorNums(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        # Using a list to store counts of numbers 1 to n
-        counts = [0] * (n + 1)
-        
-        for num in nums:
-            counts[num] += 1
-            
-        duplicate = -1
-        missing = -1
-        
-        for i in range(1, n + 1):
-            if counts[i] == 2:
-                duplicate = i
-            elif counts[i] == 0:
-                missing = i
-                
-        return [duplicate, missing]
+    def findErrorNums(self, nums):
+        n = len(nums)  # [1, 2, 2, 4]  # 4
+        actual_sum = sum(nums)  # 1 + 2 + 2 + 4 = 9
+        unique_sum = sum(set(nums))  # sum({1, 2, 4}) = 7
+        expected_sum = n * (n + 1) // 2  # 4 * (5) // 2 = 10
+
+        duplicate = actual_sum - unique_sum  # 9 - 7 = 2
+        missing = expected_sum - unique_sum  # 10 - 7 = 3
+        return [duplicate, missing]  # [2, 3]
         
