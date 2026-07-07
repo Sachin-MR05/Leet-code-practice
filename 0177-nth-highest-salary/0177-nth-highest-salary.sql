6b630114aa34d5c5f4c90721f6af497f932eee4b
SACHIN MR <221297219+Sachin-MR05@users.noreply.github.com>
2026-07-07 15:43:31 +0530

    463 ms (54.03%) 0B (100%) 0177-nth-highest-salary .sql Medium

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,11 +1,21 @@
-CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
-BEGIN
-set n =N -1;
-  RETURN (
-      SELECT DISTINCT salary 
-      FROM Employee 
-      ORDER BY salary DESC 
-      LIMIT 1 OFFSET N
+-- CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+-- BEGIN
+-- set n =N -1;
+--   RETURN (
+--       SELECT DISTINCT salary 
+--       FROM Employee 
+--       ORDER BY salary DESC 
+--       LIMIT 1 OFFSET N
 
-  );
-END
+--   );
+-- END
+
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+begin
+set n = n-1;
+return(
+    select distinct salary from Employee
+    order by salary desc limit 1 offset N
+);
+end
