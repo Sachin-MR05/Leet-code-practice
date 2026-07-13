6e906395db88e7f59990e56c31121bc2482fdf08
SACHIN MR <221297219+Sachin-MR05@users.noreply.github.com>
2026-07-13 14:35:10 +0530

    1649 ms (5.29%) 0B (100%) 0185-department-top-three-salaries .sql Hard

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select d.name as Department , e.name as Employee, e.salary as Salary from employee e left join department d on e.departmentid = d.id where 3 > (select count(distinct salary) from employee where departmentid = e.departmentid and salary > e.salary );
